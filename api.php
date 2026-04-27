d614dc8de2ca96bea59950f2857cc3abf8ffcfbc
Igazi1 <szegletesdavid@gmail.com>
2026-04-27 20:06:49 +0200

    api adatb létrehozva

---
+++ api.php
@@ -0,0 +1,4 @@
+<?php
+$dbh = new PDO('mysql:host=mysql.nethely.hu;dbname=adatbwebbead', 'adatbwebbead', 'Admin123123',
+                            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+ ?>
