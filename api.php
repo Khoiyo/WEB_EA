e5b79431ba14f86565bbcc34d5e107658fe51248
Igazi1 <szegletesdavid@gmail.com>
2026-04-30 13:09:14 +0200

    adatbázis host modositva

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-$host = 'mysql.nethely.hu';
+$host = 'localhost';
 $dbname = 'adatbwebbead'; 
 $user = 'adatbwebbead';  
 $pass = 'Admin123123';    
