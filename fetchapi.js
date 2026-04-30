a0e097643fb9c4536ce31b481f8449a37176a002
Khoiyo <nagylacicsak1@gmail.com>
2026-04-30 11:54:53 +0200

    új pizza hozzáadása

--- fetchapi.js
+++ fetchapi.js
@@ -7,3 +7,12 @@ async function getPizzas() {
         console.error('Hiba a lekérés során:', error);
     }
 }
+
+async function addPizza(pizzaData) {
+    await fetch('backend/pizza_api.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(pizzaData)
+    });
+    getPizzas(); 
+}
